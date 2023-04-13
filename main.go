e0c0d6637e60b4eaf47c7c89763fc99b1643e1f7
jrodonnell <james.riley.odonnell@gmail.com>
2023-04-13 14:43:18 -0600

    did some learnding about packages and modules

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import query "query/pkg"
+import (
+	"learn/pkg"
+)
 
 func main() {
-	query.Get("/home/jrodonnell/.kube/config.master", "cattle-system", "pod", "rancher-5f9b79d5fc-x89zh")
+	pkg.Get("/home/jrodonnell/.kube/config.master", "cattle-system", "pod", "rancher-5f9b79d5fc-x89zh")
 }
--- pkg/query.go
+++ pkg/query.go
@@ -1,4 +1,4 @@
-package query
+package pkg
 
 import (
 	"context"
